24e0b06d5ba0555adac116c754d577e0de252853
Taha Siddiqui <tsiddiqui@touchbistro.com>
2023-10-16 11:47:55 -0500

    main code

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,37 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import decompress from 'decompress';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import sqlite from 'sqlite3';
+import fs from 'fs';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+const dataDirectory = path.resolve(`${__dirname}/../../data`);
+const tempFileDirectory = path.resolve(`${__dirname}/../../data/temp`);
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
+const files = fs.readdirSync(dataDirectory).filter(f => f.endsWith('.zip'));
+
+for (const file of files) {
+  console.log(file);
+  decompress(`${dataDirectory}/${file}`, `${tempFileDirectory}/unzipped`)
+    .then((files: { path: string; data: Buffer }[]) => {
+      const sqlFile = files.find((f) => f.path.endsWith('.sql'));
+      fs.writeFileSync(`${tempFileDirectory}/temp.sql`, sqlFile.data);
+
+      const db = new sqlite.Database(`${tempFileDirectory}/temp.sql`);
+      db.all('select * from zwaiter', function (_err, rows) {
+        console.log('zwaiter', rows);
+      });
+      db.all('select * from zrole', function (_err, rows) {
+        console.log('zrole', rows);
+      });
+      db.all('select * from zwaiterrole', function (_err, rows) {
+        console.log('zwaiterrole', rows);
+      });
+      fs.rmSync(`${tempFileDirectory}/unzipped`, { recursive: true, force: true })
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
